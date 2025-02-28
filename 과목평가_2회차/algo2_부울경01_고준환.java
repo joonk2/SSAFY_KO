156f4abe0d61943af8465e8c2917de3fcaf96375
joonk2 <with.joonk@gmail.com>
2025-02-28 22:58:53 +0900

    Update algo2_부울경01_고준환.java

--- 과목평가_2회차/algo2_부울경01_고준환.java
+++ 과목평가_2회차/algo2_부울경01_고준환.java
@@ -1,98 +1,3 @@
-package ttt;
-
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-//import java.util.Arrays;
-
-public class test {
-	static int[] dx = {1, 0, -1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1; tc < T+1; tc++) {
-			int monster = Integer.parseInt(br.readLine());
-			int N = 10;
-			
-			int[][] matrix = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				String line = br.readLine();
-				for (int j = 0; j < N; j++) {
-					matrix[i][j] = line.charAt(j) - '0';
-				}
-			}
-			
-			
-			
-			for (int y = 0; y < N; y++) {
-				for (int x = 0; x < N; x++) {
-					int target = matrix[y][x];
-					
-					
-					if ( 1 <= target && target <= 3) {
-						for (int d = 0; d < 4; d++) {
-							for (int k = 1; k < target + 1; k++) {
-								int nx = (dx[d] * k) + x;
-								int ny = (dy[d] * k) + y;
-								
-								// cond-1
-								if (!  (0 <= ny && ny < N && 0 <= nx && nx < N) ) {
-									break;
-								}
-								
-								// cond-2
-								if ( (1 <= matrix[ny][nx] && matrix[ny][nx] <= 3) || (matrix[ny][nx] == 4)   ) {
-									break;
-								}
-								
-								// cond-3
-								// distinguish  0 --> 123
-								if (matrix[ny][nx] == 0) {
-									matrix[ny][nx] = 123;
-								}
-								
-								
-							}
-						}
-					}
-					
-					
-					
-				}
-			}
-			
-			
-			int cnt = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (matrix[i][j] == 0) {
-						cnt++;
-					}
-				}
-			}
-			
-			System.out.println("#" + tc + " " + cnt);
-			
-			
-			
-//			System.out.println(Arrays.deepToString(matrix));
-			
-			
-			
-		}
-		
-		
-	}
-}
-
-
-
-
-
 /*
 2
 3
@@ -118,7 +23,100 @@ public class test {
 0400000010
 0000000000
 
- 
- #1 73
- #2 42
+
+#1 73
+#2 42
 */
+
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//import java.util.Arrays;
+
+public class Day1 {
+	static int[] dx = {0,-1,0,1};
+	static int[] dy = {1,0,-1,0};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc < T+1; tc++) {
+			int monster = Integer.parseInt(br.readLine());
+			int N = 10;
+			
+			int[][] matrix = new int[N][N];
+			
+			for (int y = 0; y < N; y++) {
+				String col = br.readLine();
+				for (int x = 0; x < N; x++) {
+					matrix[y][x] = col.charAt(x) - '0';
+				}
+			}
+			
+			
+			for (int y = 0; y < N; y++) {
+				for (int x = 0; x < N; x++) {
+					int target = matrix[y][x];
+					
+					// 괴물 1 ~ 3
+					if (1 <= target && target <= 3) {
+						for (int d = 0; d < 4; d++) {
+							for (int k = 1; k < target + 1; k++) {
+								int ny = (dy[d]*k) + y;
+								int nx = (dx[d]*k) + x;
+								
+								// cond-1 범위 밖이면 break
+								if (ny < 0 || ny > N-1 || nx < 0 || nx > N-1) {
+									break;
+								}
+								
+								// cond-2 괴물을 만나도 break
+								if ( (1 <= matrix[ny][nx] && matrix[ny][nx] <= 3)  || (matrix[ny][nx] == 4) ) {
+									break;
+								}
+								
+								// cond-3 괴물의 광선이 비춰지는 곳 0 --> 123;
+								if (matrix[ny][nx] == 0) {
+									matrix[ny][nx] = 123;
+								}
+								
+								
+							}
+						}
+					}
+					
+					
+				}
+			}
+			
+			
+			
+			int cnt = 0;
+			for (int y = 0; y < N; y++) {
+				for (int x = 0; x < N; x++) {
+					if (matrix[y][x] == 0) {
+						cnt++;
+					}
+				}
+			}
+			
+			System.out.println("#" + tc + " "+ cnt);
+			
+//			System.out.println(Arrays.deepToString(matrix));
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+}
